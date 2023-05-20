43f7078bc029183a6f815bb2944642d8b3e394ff
mehdi <mehdiarghan@gmail.com.git/>
2023-05-20 20:58:18 +0330

    - added Person class - changed Main class

--- src/main/java/mavanone/Main.java
+++ src/main/java/mavanone/Main.java
@@ -2,6 +2,9 @@ package mavanone;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello maven");
+        Person person=new Person();
+        person.setFirstName("mehdi");
+        person.setLastName("arghan");
+        System.out.println(person.getFirstName()+" "+person.getLastName());
     }
 }
